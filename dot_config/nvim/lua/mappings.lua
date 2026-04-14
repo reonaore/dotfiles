d8563a9543fcd856894651e5f3858bd12ce9eb2f
onare <reona.shiode@gmail.com>
2026-04-14 12:19:27 +0900

    Install markdown preview plugin

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -59,12 +59,13 @@ else
   map("n", "gi", vim.lsp.buf.implementation, { noremap = true, desc = "LSP Go to implementation" })
   map("n", "gd", vim.lsp.buf.definition, { noremap = true, desc = "LSP Go to definition" })
   map("n", "<C-o><C-r>", vim.lsp.buf.rename, { noremap = true, desc = "Rename symbol" })
-  map("n", "<C-o><C-f>", "<cmd>FzfLua live_grep<CR>", { noremap = true, desc = "Find Text" })
-  map("n", "<C-o><C-g>", "<cmd>FzfLua git_status<CR>", { noremap = true, desc = "Git Status" })
-  map("n", "<leader>gt", "<cmd>FzfLua git_status<CR>", { noremap = true, desc = "Git Status" })
-  map("n", "<leader>fw", "<cmd>FzfLua live_grep<CR>", { noremap = true, desc = "Find Text" })
-  map("n", "<leader>ff", "<cmd>FzfLua files<CR>", { noremap = true, desc = "Find Files" })
   map("n", "<C-o><C-e>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+  map("n", "<leader>gt", "<cmd>FzfLua git_status<CR>", { noremap = true, desc = "Git Status" })
+  map("n", "<C-o><C-g>", "<cmd>FzfLua git_status<CR>", { noremap = true, desc = "Git Status" })
+  map("n", "<leader>fw", "<cmd>FzfLua live_grep<CR>", { noremap = true, desc = "Find Text" })
+  map("n", "<C-o><C-f>", "<cmd>FzfLua live_grep<CR>", { noremap = true, desc = "Find Text" })
+  map("n", "<leader>ff", "<cmd>FzfLua files<CR>", { noremap = true, desc = "Find Files" })
+  map("n", "<C-o><C-o>", "<cmd>FzfLua commands<CR>", { noremap = true, desc = "Find Commands" })
 end
 
 if vim.g.vscode then
---
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+  keys = {
+    { '<leader>mp', '<cmd>RenderMarkdown toggle<cr>', desc = 'Toggle markdown preview' },
+  },
+}
