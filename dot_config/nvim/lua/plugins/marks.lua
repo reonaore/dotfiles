bb7d51bbc7cde9c3962b093adfee5adcc84ab2c6
onare <reona.shiode@gmail.com>
2026-04-14 14:01:08 +0900

    add plugin for marks

---
+++ dot_config/nvim/lua/plugins/marks.lua
@@ -0,0 +1,13 @@
+return {
+  "chentoast/marks.nvim",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      delete = "dm",
+      toggle = "mm",
+      clear = "mM",
+      next = "m]",
+      prev = "m[",
+    }
+  },
+}
