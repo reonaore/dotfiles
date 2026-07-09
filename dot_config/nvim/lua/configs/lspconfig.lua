a2e60479d7a2cab03bbd3e4aa37eb6202ac1ea0b
onare <reona.shiode@gmail.com>
2026-07-09 18:32:23 +0900

    enable kotlin_lsp

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua_ls", "yamlls", "ty" }
+local servers = { "html", "cssls", "lua_ls", "yamlls", "ty", "kotlin_lsp" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
